1cb33b1ee416a2df21984136c6d836ea6f8a95a4
Kirill Korobov <s057496@ad.viko.lt>
2024-09-12 11:46:09 +0300

    Pirmi du uzdaviniai

--- pirmas darbas/main.cpp
+++ pirmas darbas/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //pirma dalis
+    string Vardas = "Kirill ";
+    string Pavarde = "Korobov ";
+    int Amzius = 21 ;
+    int Grupe = 1 ;
+    int Kursas = 1 ;
+    string x ="Programu sistemos ";
+    cout <<"Studento vardas " << Vardas << endl;
+    cout <<"Studento pavarde " << Pavarde << endl;
+cout <<"Studento amzius "<< Amzius<< endl;
+    cout <<" Grupe "<< Grupe<< endl;
+    cout <<" Kursas "<< Kursas<< endl;
+    cout <<" Studiju programos pavadinimas " << x<<endl;
+    cout <<"\n"<<endl;
+    cout <<"---------------------------------- " <<endl;
+    //antra  dalis
+    string pavadinimas = "FK Zalgiris";
+    int metai = 1947;
+    string vadova = "Vilma Venslovaitiene";
+ string arena ="LFF stadionas";
+    int vietos = 5067;
+    cout <<" Komandos pavadinimas  "<< pavadinimas<< endl;
+    cout <<" Ikurimo metai "<< metai<< endl;
+    cout <<" Klubo Vadove "<< vadova<< endl;
+    cout <<" Klubo arena "<< arena<< endl;
+    cout <<" Vietu skaicius "<< vietos<< endl;
     return 0;
 }
+
+
