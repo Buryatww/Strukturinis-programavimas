b4cce903a60eea8ec99a5f6e4cc0a8d9f7d107aa
Kirill Korobov <s057496@ad.viko.lt>
2024-09-19 11:43:44 +0300

    antras darbas

---
+++ antras darbas/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main() {
+   //pirmas-antras darbas
+    // int q,w,e,r,t,y;
+    // int vidurkis;
+    // cout<<"Iveskite studentu pazymius "<<endl;
+    // cin>>q>>w>>e>>r>>t>>y;
+    // vidurkis=(q+w+e+r+t+y)/6;
+    // cout<<"Vidurkis ligus "<<vidurkis<<endl;
+    // if (vidurkis>=5) {
+    //     cout<<"Vidurkis teigiamas"<<endl;
+    // }
+    // else{cout<<"Vidurkis neigiamas"<<endl;}
+    //trecias darbas
+ //    int u;
+ //    cout<<"Iveskite egzamino pazymi "<<endl;
+ //    cin>>u;
+ // if (u==10){
+ //     cout<<"Puiku"<<endl;
+ // }
+ //    else if (u<=9) {
+ //        cout<<"labai gerai"<<endl;
+ //    }
+ //    else if (u<=7) {
+ //        cout<<"gerai"<<endl;
+ //    }
+ //    else if (u<=5) {
+ //        cout<<"patenkinamai"<<endl;
+ //    }
+ //    else if (u>=5) {
+ //        cout<<"egzaminas neislaikytas"<<endl;
+ //    }
+    //ketvirtas darbas
+
+     return 0;
+}
