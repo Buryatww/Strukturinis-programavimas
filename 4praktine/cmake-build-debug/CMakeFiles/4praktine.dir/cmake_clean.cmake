29eeb125fd70811782dea639ceaba682121d56bb
Kirill Korobov <s057496@ad.viko.lt>
2024-12-12 10:55:23 +0200

    failas

---
+++ 4praktine/cmake-build-debug/CMakeFiles/4praktine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "4praktine.exe"
+  "4praktine.exe.manifest"
+  "4praktine.pdb"
+  "CMakeFiles/4praktine.dir/main.cpp.obj"
+  "CMakeFiles/4praktine.dir/main.cpp.obj.d"
+  "lib4praktine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4praktine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
